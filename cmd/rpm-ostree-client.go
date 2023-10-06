dfa1bd5afaef6b4ca4c8c99646334afe86ed1f1d
Leo Ochoa <lochoa@redhat.com>
2023-10-06 11:42:45 +0200

    Refactor rpm-ostree-client to add support for runInHostNamespace

--- cmd/rpm-ostree-client.go
+++ cmd/rpm-ostree-client.go
@@ -14,20 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This client lifts code from: https://github.com/coreos/rpmostree-client-go/blob/main/pkg/client/client.go
+
 package cmd
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
-	"os/exec"
-	"strconv"
-	"strings"
 
-	"github.com/containers/image/v5/docker/reference"
-	yaml "gopkg.in/yaml.v3"
-
-	"github.com/coreos/rpmostree-client-go/pkg/imgref"
+	"gopkg.in/yaml.v3"
 )
 
 // Status summarizes the current worldview of the rpm-ostree daemon.
@@ -59,7 +54,7 @@ type Deployment struct {
 	Unlocked                *string  `json:"unlocked"`
 }
 
-// Client is a handle for interacting with an rpm-ostree based system.
+// Client is a handle for interacting with a rpm-ostree based system.
 type Client struct {
 	clientid string
 }
@@ -73,15 +68,9 @@ func NewClient(id string) Client {
 	}
 }
 
-func (client *Client) newCmd(args ...string) *exec.Cmd {
-	r := exec.Command("rpm-ostree", args...)
-	r.Env = append(r.Env, "RPMOSTREE_CLIENT_ID", client.clientid)
-	return r
-}
-
-func (client *Client) run(args ...string) error {
-	c := client.newCmd(args...)
-	return c.Run()
+func (client *Client) newCmd(args ...string) []byte {
+	rawOutput, _ := runInHostNamespace("rpm-ostree", args...)
+	return rawOutput
 }
 
 // VersionData represents the static information about rpm-ostree.
@@ -97,13 +86,10 @@ type rpmOstreeVersionData struct {
 
 // RpmOstreeVersion returns the running rpm-ostree version number
 func (client *Client) RpmOstreeVersion() (*VersionData, error) {
-	c := client.newCmd("--version")
-	buf, err := c.Output()
-	if err != nil {
-		return nil, err
-	}
+	buf := client.newCmd("--version")
 
 	var q rpmOstreeVersionData
+
 	if err := yaml.Unmarshal(buf, &q); err != nil {
 		return nil, fmt.Errorf("failed to parse `rpm-ostree --version` output: %w", err)
 	}
@@ -111,190 +97,14 @@ func (client *Client) RpmOstreeVersion() (*VersionData, error) {
 	return &q.Root, nil
 }
 
-func compareVersionStrings(required, actual string) (bool, error) {
-	verparts := strings.Split(actual, ".")
-	verlen := len(verparts)
-	requiredparts := strings.Split(required, ".")
-	for i, req := range requiredparts {
-		if i >= verlen {
-			break
-		}
-		reqv, err := strconv.Atoi(req)
-		if err != nil {
-			return false, err
-		}
-		actualv, err := strconv.Atoi(verparts[i])
-		if err != nil {
-			return false, err
-		}
-		if actualv < reqv {
-			return false, nil
-		}
-	}
-	return true, nil
-}
-
-// RpmOstreeVersionEqualOrGreater checks whether the version of rpm-ostree is new enough.
-func (client *Client) RpmOstreeVersionEqualOrGreater(required string) (bool, error) {
-	vdata, err := client.RpmOstreeVersion()
-	if err != nil {
-		return false, err
-	}
-
-	return compareVersionStrings(required, vdata.Version)
-}
-
 // QueryStatus loads the current system state.
 func (client *Client) QueryStatus() (*Status, error) {
 	var q Status
-	c := client.newCmd("status", "--json")
-	buf, err := c.Output()
-	if err != nil {
-		return nil, err
-	}
+	buf := client.newCmd("status", "--json")
 
 	if err := json.Unmarshal(buf, &q); err != nil {
 		return nil, fmt.Errorf("failed to parse `rpm-ostree status --json` output: %w", err)
 	}
 
 	return &q, nil
-}
-
-// GetBootedDeployment finds the booted deployment, or returns an error if none is found.
-func (s *Status) GetBootedDeployment() (*Deployment, error) {
-	for num := range s.Deployments {
-		deployment := s.Deployments[num]
-		if deployment.Booted {
-			return &deployment, nil
-		}
-	}
-	return nil, fmt.Errorf("no booted deployment found")
-}
-
-// GetStagedDeployment finds the staged deployment, or returns nil if none is found.
-func (s *Status) GetStagedDeployment() *Deployment {
-	for num := range s.Deployments {
-		deployment := s.Deployments[num]
-		if deployment.Staged {
-			return &deployment
-		}
-	}
-	return nil
-}
-
-// GetBaseChecksum returns the ostree commit used as a base.
-func (s *Deployment) GetBaseChecksum() string {
-	if s.BaseChecksum != nil {
-		return *s.BaseChecksum
-	}
-	return s.Checksum
-}
-
-// Parse the deployment's container image reference.
-func (d *Deployment) RequireContainerImage() (*imgref.OstreeImageReference, error) {
-	if d.ContainerImageReference == "" {
-		return nil, fmt.Errorf("deployment is not using a container origin")
-	}
-	return imgref.Parse(d.ContainerImageReference)
-}
-
-// Remove the pending deployment.
-func (client *Client) RemovePendingDeployment() error {
-	return client.run("cleanup", "-p")
-}
-
-// Remove the rollback deployment.
-func (client *Client) RemoveRollbackDeployment() error {
-	return client.run("cleanup", "-r")
-}
-
-// ChangeKernelArguments adjusts the kernel arguments.
-func (client *Client) ChangeKernelArguments(toAdd []string, toRemove []string) error {
-	args := []string{"kargs"}
-	for _, arg := range toRemove {
-		args = append(args, "--delete="+arg)
-	}
-	for _, arg := range toAdd {
-		args = append(args, "--append="+arg)
-	}
-	return client.run(args...)
-}
-
-// ChangePackages installs or removes packages.
-func (client *Client) ChangePackages(toAdd []string, toRemove []string) error {
-	args := []string{}
-	if len(toAdd) == 0 {
-		args = append(args, "uninstall")
-		args = append(args, toRemove...)
-		for _, pkg := range toAdd {
-			args = append(args, "--install")
-			args = append(args, pkg)
-		}
-	} else {
-		args = append(args, "install")
-		args = append(args, toAdd...)
-		for _, pkg := range toRemove {
-			args = append(args, "--uninstall")
-			args = append(args, pkg)
-		}
-	}
-	return client.run(args...)
-}
-
-// OverrideRemove uninstalls base packages, optionally installing new ones at the same time.
-func (client *Client) OverrideRemove(toRemove []string, toInstall []string) error {
-	args := []string{"override", "remove"}
-	args = append(args, toRemove...)
-	for _, pkg := range toInstall {
-		args = append(args, "--install")
-		args = append(args, pkg)
-	}
-	return client.run(args...)
-}
-
-// OverrideRemove drops overrides, optionally uninstalling new ones at the same time.
-func (client *Client) OverrideReset(toReset []string, toUninstall []string) error {
-	args := []string{"override", "reset"}
-	args = append(args, toReset...)
-	for _, pkg := range toUninstall {
-		args = append(args, "--uninstall")
-		args = append(args, pkg)
-	}
-	return client.run(args...)
-}
-
-// RebaseToContainerImage switches to the target container image
-func (client *Client) RebaseToContainerImage(target reference.Reference) error {
-	return client.run("rebase", fmt.Sprintf("ostree-image-signed:docker://%s", target.String()))
-}
-
-// RebaseToContainerImageAllowUnsigned switches to the target container image, ignoring lack of image signatures.
-func (client *Client) RebaseToContainerImageAllowUnsigned(target reference.Reference) error {
-	return client.run("rebase", fmt.Sprintf("ostree-unverified-registry:%s", target.String()))
-}
-
-func parseDeploymentError(buf string) (*string, error) {
-	scanner := bufio.NewScanner(strings.NewReader(buf))
-	for scanner.Scan() {
-		msg := scanner.Text()
-		// Brutal hack: look for error:
-		// In the future, it probably makes sense to build on
-		// ostree-boot-complete
-		// https://github.com/ostreedev/ostree/pull/2589
-		// but this is backwards compatible.
-		if !strings.HasPrefix(msg, "error:") {
-			continue
-		}
-		return &msg, nil
-	}
-	return nil, nil
-}
-
-// QueryPreviousDeploymentError searches from an error message
-func (client *Client) QueryPreviousDeploymentError() (*string, error) {
-	bufraw, err := exec.Command("journalctl", "-b", "-1", "-o", "cat", "_UID=0", "-u", "ostree-finalize-staged.service").CombinedOutput()
-	if err != nil {
-		return nil, fmt.Errorf("error querying for previous finalization via journalctl: %w", err)
-	}
-	return parseDeploymentError(string(bufraw))
 }
