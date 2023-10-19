c52307892b57eca9b0e3e8ef086f77cd884ee524
Leo Ochoa <lochoa@redhat.com>
2023-10-19 22:56:50 +0200

    Update installation_configuration_files scripts
    Make installation_configuration_files scripts compliant with shellcheck and bashate.

--- installation_configuration_files/scripts/prepare-installation-configuration.sh
+++ installation_configuration_files/scripts/prepare-installation-configuration.sh
@@ -8,39 +8,38 @@ mkdir -p /opt/openshift
 cd /opt/openshift
 
 function mount_config {
-  echo "Mounting config iso"
-  mkdir -p /mnt/config
-  if [[ ! $(mountpoint --quiet /mnt/config) ]]; then
-      mount "/dev/$1" /mnt/config
-  fi
-  ls /mnt/config
+    echo "Mounting config iso"
+    mkdir -p /mnt/config
+    if [[ ! $(mountpoint --quiet /mnt/config) ]]; then
+        mount "/dev/$1" /mnt/config
+    fi
+    ls /mnt/config
 }
 
 function umount_config {
-  echo "Unmounting config iso"
-  umount /dev/$1
-  rm -rf /mnt/config
+    echo "Unmounting config iso"
+    umount /dev/$1
+    rm -rf /mnt/config
 }
 
 CONFIG_PATH=/opt/openshift/
 NETWORK_CONFIG_PATH=/opt/openshift/network-configuration
 
 echo "Waiting for ${CONFIG_PATH}"
-while [[ ! $(lsblk -f --json | jq -r '.blockdevices[] | select(.label == "relocation-config") | .name') && ! -d "${CONFIG_PATH}" ]];
-do
-  echo "Waiting for site-config"
-  sleep 5
+while [[ ! $(lsblk -f --json | jq -r '.blockdevices[] | select(.label == "relocation-config") | .name') && ! -d "${CONFIG_PATH}" ]]; do
+    echo "Waiting for site-config"
+    sleep 5
 done
 
 DEVICE=$(lsblk -f --json | jq -r '.blockdevices[] | select(.label == "relocation-config") | .name')
 if [[ -n "${DEVICE}" && ! -d "${CONFIG_PATH}" ]]; then
-  mount_config "${DEVICE}"
-  cp -r /mnt/config/* ${CONFIG_PATH}
+    mount_config "${DEVICE}"
+    cp -r /mnt/config/* ${CONFIG_PATH}
 fi
 
 if [ ! -d "${CONFIG_PATH}" ]; then
-  echo "Failed to find cluster configuration at ${CONFIG_PATH}"
-  exit 1
+    echo "Failed to find cluster configuration at ${CONFIG_PATH}"
+    exit 1
 fi
 
 echo "${CONFIG_PATH} has been created"
@@ -48,9 +47,9 @@ echo "${CONFIG_PATH} has been created"
 set +o allexport
 
 if [[ -d "${NETWORK_CONFIG_PATH}" ]]; then
-  echo "Static network configuration exist"
-  cp "${NETWORK_CONFIG_PATH}"/*.nmconnection /etc/NetworkManager/system-connections/ -f
-  systemctl restart NetworkManager
+    echo "Static network configuration exist"
+    cp "${NETWORK_CONFIG_PATH}"/*.nmconnection /etc/NetworkManager/system-connections/ -f
+    systemctl restart NetworkManager
   # TODO: we might need to delete the connection first
 else
     echo "Static network configuration do not exist"
@@ -58,5 +57,5 @@ fi
 systemctl disable prepare-installation-configuration.service
 
 if [[ -n "${DEVICE}" ]]; then
-  umount_config "${DEVICE}"
+    umount_config "${DEVICE}"
 fi
